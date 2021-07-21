00f0b894b21346bae90e82c2ae550c5be57cf7db
Ashish <73551845+Ashish2468@users.noreply.github.com>
2021-07-21 20:09:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function renderCafe(doc){
     cross.addEventListener('click', (e) => {
         e.stopPropagation();
         let id = e.target.parentElement.getAttribute('data-id');
-       // db.collection('cafes').doc(id).delete();
+
     });
 }
 
