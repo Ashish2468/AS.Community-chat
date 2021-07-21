65eff1645e9885e690d4e094184a4a0c05b92619
Ashish <73551845+Ashish2468@users.noreply.github.com>
2021-07-21 20:08:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const cafeList = document.querySelector('#cafe-list');
 const form = document.querySelector('#add-cafe-form');
 
-// create element & render cafe
 function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
